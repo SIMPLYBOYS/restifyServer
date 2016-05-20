4e51918cc06db203f2af8f343e7f965c5c5995e1
SIMPLYBOYS <ferrari828@gmail.com>
2016-05-20 17:22:16 +0800

    update Remover

---
+++ update/Remover.js
@@ -0,0 +1,52 @@
+var http = require('http');
+var cheerio = require('cheerio');
+var util = require('util');
+var config = require('../config');
+var dbIMDB = config.dbIMDB;
+var EventEmitter = require('events').EventEmitter;
+var STATUS_CODES = http.STATUS_CODES;
+/*
+ * Scraper Constructor
+**/
+function Remover (title, position) {
+    this.title = title;
+    this.position = position;
+    this.init();
+}
+/*
+ * Make it an EventEmitter
+**/
+util.inherits(Remover, EventEmitter);
+
+/*
+ * Initialize scraping
+**/
+Remover.prototype.init = function () {
+    this.on('updated', function (title) {
+        console.log('\n====> \"'+title + '\" got updated!!!');
+        this.emit('complete', title);
+    });
+    this.updateMovie();
+};
+
+/*
+ * Parse html and return an object
+**/
+Remover.prototype.updateMovie = function () {
+  
+  var that = this;
+
+  dbIMDB.imdb.findOne({'title': that['title']}, function(err, doc) {
+
+      if (!doc) {
+        console.log('\n\n' + that['title'] + 'not found!');
+        return;     
+      }
+
+      dbIMDB.imdb.update({'title': that['title']}, {'$unset': {'top': 1}}, function() {
+        that.emit('updated', that.title);
+      });
+  })
+};
+
+module.exports = Remover;
