5fbd176290d37e18cef46ebbab4707f0ab043b6c
SIMPLYBOYS <ferrari828@gmail.com>
2016-08-07 17:03:34 +0800

    refactor MovieInformer module

--- MovieInfomer.js
+++ MovieInfomer.js
@@ -28,10 +28,9 @@ util.inherits(MovieInformer, EventEmitter);
 **/
 MovieInformer.prototype.init = function () {
     var self = this;
-    console.log('init MovieInformer ' + self.title);
+    console.log('init MovieInformer ====>' + self.title);
     self.on('finish', function (movieInfo) {
         console.log('parse finish');
-        // console.log(movieInfo);
         var info = JSON.parse(movieInfo);
             info = info[0];
         console.log(info);
@@ -39,11 +38,26 @@ MovieInformer.prototype.init = function () {
         async.series([
             function (done) {
               if (self.title == 'Ben-Hur') {
-                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')}, {'$set': {'detailContent': {
-                      summery: info['simplePlot'],
-                      country: info['countries'],
-                      rated: info['rated']
-                  }}
+                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')}, {$set: {
+                    detailContent: {
+                        summery: info['simplePlot'],
+                        country: info['countries'],
+                        rated: info['rated']
+                    },
+                    plot: info['plot'],
+                    genres: info['genres'],
+                    metascore: info['metascore'],
+                    rating: {
+                        score: info['rating'],
+                        votes: info['votes']
+                    },
+                    runtime: info['runtime'],
+                    directors: info['directors'],
+                    writers: info['writers'],
+                    idIMDB: info['idIMDB'],
+                    releaseDate: parseInt(info['releaseDate']),
+                    year: parseInt(info['year'])
+                  }
                 },
                   function(err, doc) {
                     console.log(err);
@@ -51,209 +65,36 @@ MovieInformer.prototype.init = function () {
                     done(null);
                 });
               } else {
-                dbIMDB.imdb.update({'title': self.title}, {'$set': {'detailContent': {
-                      summery: info['simplePlot'],
-                      country: info['countries'],
-                      rated: info['rated']
-                  }}
+                dbIMDB.imdb.update({'title': self.title}, {$set: {
+                    detailContent: {
+                        summery: info['simplePlot'],
+                        country: info['countries'],
+                        rated: info['rated']
+                    },
+                    plot: info['plot'],
+                    genres: info['genres'],
+                    metascore: info['metascore'],
+                    rating: {
+                        score: info['rating'],
+                        votes: info['votes']
+                    },
+                    runtime: info['runtime'],
+                    directors: info['directors'],
+                    writers: info['writers'],
+                    idIMDB: info['idIMDB'],
+                    releaseDate: parseInt(info['releaseDate']),
+                    year: parseInt(info['year'])
+                  }
                 }, function(err, doc){
                   console.log(err);
-                  console.log('step1');
                   done(null);
                 });
               }
             },
-            function (done) {
-              if (self.title == 'Ben-Hur') {
-                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')}, {'$set': {'plot': info['plot']
-                  }
-                }, function(err, doc) {
-                  console.log('step2');
-                  done(null);
-                });
-              } else {
-                dbIMDB.imdb.update({'title': self.title}, {'$set': {'plot': info['plot']
-                  }
-                }, function() {
-                  console.log('step2');
-                  done(null);
-                });
-              }
-            },
-            function (done) {
-              if (self.title == 'Ben-Hur') {
-                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')},  {'$set': {'genres': info['genres']
-                  }
-                }, function(err, doc) {
-                  console.log('step3');
-                  done(null);
-                });
-              } else {
-                dbIMDB.imdb.update({'title': self.title}, {'$set': {'genres': info['genres']
-                  }
-                }, function() {
-                  console.log('step3');
-                  done(null);
-                });
-              }
-            },
-            function (done) {
-              if (self.title == 'Ben-Hur') {
-                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')}, {'$set': {'metascore': info['metascore']
-                  }
-                }, function() {
-                  console.log('step4');
-                  done(null);
-                });
-              } else {
-                dbIMDB.imdb.update({'title': self.title}, {'$set': {'metascore': info['metascore']
-                  }
-                }, function() {
-                  console.log('step4');
-                  done(null);
-                });
-              }
-            },
-            function (done) {
-              if (self.title == 'Ben-Hur') {
-                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')}, {'$set': {'rating': info['rating']
-                  }
-                }, function() {
-                  console.log('step5');
-                  done(null);
-                });
-              } else {
-                dbIMDB.imdb.update({'title': self.title}, {'$set': {'rating': info['rating']
-                  }
-                }, function() {
-                  console.log('step5');
-                  done(null);
-                });
-              }
-            },
-            function (done) {
-              if (self.title == 'Ben-Hur') {
-                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')},{'$set': {'votes': info['votes']
-                  }
-                }, function() {
-                  console.log('step6');
-                  done(null);
-                });
-              } else {
-                dbIMDB.imdb.update({'title': self.title}, {'$set': {'votes': info['votes']
-                  }
-                }, function() {
-                  console.log('step6');
-                  done(null);
-                });
-              }
-            },
-            function (done) {
-              if (self.title == 'Ben-Hur') {
-                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')}, {'$set': {'runtime': info['runtime']
-                  }
-                }, function(){
-                  console.log('step7');
-                  done(null);
-                });
-              } else {
-                dbIMDB.imdb.update({'title': self.title}, {'$set': {'runtime': info['runtime']
-                  }
-                }, function(){
-                  console.log('step7');
-                  done(null);
-                });
-              }
-            },
-            function (done) {
-              if (self.title == 'Ben-Hur') {
-                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')}, {'$set': {'directors': info['directors']
-                  }
-                }, function(){
-                  console.log('step8');
-                  done(null);
-                });
-              } else {
-                dbIMDB.imdb.update({'title': self.title}, {'$set': {'directors': info['directors']
-                  }
-                }, function(){
-                  console.log('step8');
-                  done(null);
-                });
-              }  
-            },
-            function (done) {
-              if (self.title == 'Ben-Hur') {
-                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')}, {'$set': {'writers': info['writers']
-                  }
-                }, function() {
-                  console.log('step9');
-                  done(null);
-                });
-              } else {
-                 dbIMDB.imdb.update({'title': self.title}, {'$set': {'writers': info['writers']
-                  }
-                }, function() {
-                  console.log('step9');
-                  done(null);
-                });
-              }
-            },
-            function (done) {
-              if (self.title == 'Ben-Hur') {
-                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')}, {'$set': {'idIMDB': info['idIMDB']
-                  }
-                }, function() {
-                  console.log('step10' + ' ==> ' + info['idIMDB']);
-                  done(null);
-                });
-              } else {
-                dbIMDB.imdb.update({'title': self.title}, {'$set': {'idIMDB': info['idIMDB']
-                  }
-                }, {multi: true}, function() {
-                  console.log('step10' + ' ==> ' + info['idIMDB']);
-                  done(null);
-                });
-              }
-            },
-            function (done) {
-              if (self.title == 'Ben-Hur') {
-                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')}, {'$set': {'releaseDate': parseInt(info['releaseDate'])
-                  }
-                }, function() {
-                  console.log('step11');
-                  done(null);
-                });
-              } else {
-                dbIMDB.imdb.update({'title': self.title}, {'$set': {'releaseDate': parseInt(info['releaseDate'])
-                  }
-                }, function() {
-                  console.log('step11');
-                  done(null);
-                });
-              }      
-            },
-            function (done) {
-              if (self.title == 'Ben-Hur') {
-                dbIMDB.imdb.findOne({'_id': mongojs.ObjectId('5705057233c8ea8e13b62488')}, {'$set': {'year': parseInt(info['year'])
-                  }
-                }, function() {
-                  console.log('step12');
-                  done(null);
-                });
-              } else {
-                dbIMDB.imdb.update({'title': self.title}, {'$set': {'year': parseInt(info['year'])
-                  }
-                }, function() {
-                  console.log('step12');
-                  done(null);
-                });
-              } 
-            }
           ],
           function(err) {
             if (self.done) {
-              console.log('last step');
+               console.log('init MovieInformer ====>' + self.title + ' finish!!!');
               self.done(null, self.count);
             }
           });
