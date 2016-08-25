022f77da202a6cfbc55c03687a4d2e6940dddbef
SIMPLYBOYS <ferrari828@gmail.com>
2016-08-25 10:43:54 +0800

    add scroll search support

--- search/elasticsearch.js
+++ search/elasticsearch.js
@@ -4,11 +4,13 @@ var elasticClient = new elasticsearch.Client({
     host: 'localhost:9200',
     log: 'info'
 });
+exports.elasticClient = elasticClient;
 
 function searchDocument(input) {
     return elasticClient.search({
       index: 'test',
       type: 'imdb',
+      scroll: '10s',
       body: {
         query: {
           multi_match: {
