d0fd565c60bedbd20cf6dcbb35181134d9f1066b
SIMPLYBOYS <ferrari828@gmail.com>
2016-09-22 12:54:57 +0800

    update redis cache api logic

--- memory_cache/redis.js
+++ memory_cache/redis.js
@@ -1,24 +1,34 @@
 
 exports.findImdbReviewsByTitleCached = function (dbReview, redis, title, callback) {
-    redis.get(title, function (err, reply) {
-        if (err) 
+    console.log('findImdbReviewsByTitleCached ' + title);
+    redis.get(title.split(' ').join('_'), function (err, reply) {
+        if (err)
         	callback(null);
-        else if (reply) //Reviews exists in cache
-        	callback(reply);
-        else {
-            //Review doesn't exist in cache - we need to query the main database
+        else if (reply) { //Reviews exists in cache
+                var obj = JSON.parse(reply);
+                obj['review'] = obj['review'].slice(start, end);
+                console.log(obj['review'].length);
+                callback(JSON.stringify(obj));
+        } else {
+            //Review doesn't exist in cache - we need to query the main database 
             dbReview.reviews.find({title: title}, {review:1, title:1}).limit(1,
-			  function(err, doc) {
-			    console.log(doc[0]['review'].length);
-			    foo['title'] = doc[0]['title'];
-			    foo['review'] = doc[0]['review'].slice(start,end);
-			    foo['byTitle'] = false;
-			    foo['size'] = doc[0]['review'].length;
-			    var review = JSON.stringify(doc);
-			    redis.set(title, review, function () {
-                    callback(review);
+              function(err, doc) {
+                var review = JSON.stringify(doc);
+                var obj = {
+                  title: doc[0]['title'],
+                  byTitle: false,
+                  review: doc[0]['review'],
+                  size: doc[0]['review'].length
+                }
+                console.log(title.split(' ').join('_'));
+                redis.set(title.split(' ').join('_'), JSON.stringify(obj), function (err, replies) {
+                	console.log('cache done ' + err);
+                    if (!err) {
+                        obj['review'] = obj['review'].slice(start, end);
+                        callback(JSON.stringify(obj));
+                    }
                 });
-			});
+            });
         }
     });
 };
