4e88c1f9216a851caf74d3d278aeaebcd0b9c9ac
SIMPLYBOYS <ferrari828@gmail.com>
2016-11-30 23:02:34 +0800

    update Remover module

--- update/Remover.js
+++ update/Remover.js
@@ -10,6 +10,13 @@ var STATUS_CODES = http.STATUS_CODES;
 **/
 function Remover (title, position) {
     this.title = title;
+    this.infoTitle = title;
+
+    if (item.title.indexOf(',') != -1) {
+        item.title = item.title.split('(')[0];
+        item.title = item.title.split(',')[1].trim()+" "+item.title.split(',')[0]
+    }
+
     this.position = position;
     this.init();
 }
@@ -22,12 +29,92 @@ util.inherits(Remover, EventEmitter);
  * Initialize scraping
 **/
 Remover.prototype.init = function () {
+    console.log('init Remover');
     this.on('updated', function (title) {
         console.log('\n====> \"'+title + '\" got updated!!!');
         this.emit('complete', title);
     });
+
+    this.on('infoTitle_case1', function(title) {
+        this.updateMovie_InfoTitle_case1();
+    });
+
+    this.on('infoTitle_case2', function(title) {
+        this.updateMovie_InfoTitle_case2();
+    });
+
+    this.on('infoTitle_case3', function(title) {
+        this.updateMovie_InfoTitle_case3();
+    });
+
+    this.on('data not founded', function(title) {
+      console.log('\n====> \"'+title + '\" not updated!!!');
+      this.emit('complete', title);
+    })
+
     this.updateMovie();
 };
+
+Remover.prototype.updateMovie_InfoTitle_case1 = function () {
+  var that = this,
+      title = that['infoTitle'];
+
+    console.log('update by infoTitle case1 ==>');
+
+    if (title.indexOf(',') != -1) {
+        title = title.split(',')[1].trim()+" "+title.split(',')[0].toLowerCase();
+    }
+
+    dbIMDB.imdb.find({title: title}, function(err, doc) {
+        if (!doc) {
+          console.log('\n\n' + title + ' not found!');
+          that.emit('infoTitle_case2', title);
+          return;
+        }
+        dbIMDB.imdb.update({'title': title}, {'$unset': {'top': 1}}, function() {
+          that.emit('updated', that['infoTitle']);
+        });
+    }); 
+};
+
+Remover.prototype.updateMovie_InfoTitle_case2 = function () {
+    var that = this,
+        title = that['infoTitle'];
+
+    console.log('update by infoTitle case2 ==>');
+
+    dbIMDB.imdb.find({title: title}, function(err, doc) {
+        if (!doc) {
+          console.log('\n\n' + title + ' not found!');
+          that.emit('infoTitle_case3', title);
+          return;
+        }
+        dbIMDB.imdb.update({'title': title}, {'$unset': {'top': 1}}, function() {
+          that.emit('updated', that['infoTitle']);
+        });
+    }); 
+};
+
+Remover.prototype.updateMovie_InfoTitle_case3 = function () {
+    var that = this,
+        title = that['infoTitle'],
+        selector = {"title": {$regex: "/"+title.split(',')[0]+"/", $options:"i"}},
+        query = {title: new RegExp(title.split(',')[0], 'i') };
+
+    console.log('update by infoTitle case3 ==>');
+
+    dbIMDB.imdb.findOne({title: query.title}, function(err, doc) {
+        if (!doc) {
+          console.log('\n\n' + title + ' not found!');
+          that.emit('data not founded', title);
+          return;
+        }
+        dbIMDB.imdb.update({'title': title}, {'$unset': {'top': 1}}, function() {
+          that.emit('updated', that['infoTitle']);
+        });
+    });
+};
+
 
 /*
  * Parse html and return an object
@@ -39,7 +126,8 @@ Remover.prototype.updateMovie = function () {
   dbIMDB.imdb.findOne({'title': that['title']}, function(err, doc) {
 
       if (!doc) {
-        console.log('\n\n' + that['title'] + 'not found!');
+        console.log('\n\n' + that['title'] + ' not found!');
+        that.emit('infoTitle_case1', that.title);
         return;     
       }
 
